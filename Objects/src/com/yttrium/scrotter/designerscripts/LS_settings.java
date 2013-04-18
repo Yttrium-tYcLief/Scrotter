f16b53c17bf3196a8307ce7554613da07906d497
Yttrium tYcLief <tyclief@gmail.com>
2013-04-18 09:50:32 -0400

    Fix errors, add experimental settings

---
+++ Objects/src/com/yttrium/scrotter/designerscripts/LS_settings.java
@@ -0,0 +1,17 @@
+package com.yttrium.scrotter.designerscripts;
+import anywheresoftware.b4a.objects.TextViewWrapper;
+import anywheresoftware.b4a.objects.ImageViewWrapper;
+import anywheresoftware.b4a.BA;
+
+
+public class LS_settings{
+
+public static void LS_general(java.util.HashMap<String, anywheresoftware.b4a.objects.ViewWrapper<?>> views, int width, int height, float scale) {
+anywheresoftware.b4a.keywords.LayoutBuilder.setScaleRate(0.3);
+views.get("settingslist").setWidth((int)((100d / 100 * width)));
+views.get("settingslist").setHeight((int)((100d / 100 * height)));
+views.get("settingslist").setTop((int)((0d * scale)));
+views.get("settingslist").setLeft((int)((0d * scale)));
+
+}
+}
